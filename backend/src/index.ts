541a13e1887165a554622e0a1355db1cef57427d
santiago1708 <vela123tique@gmail.com>
2025-05-17 14:52:55 -0500

    configuracion del backend para el servidor

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port 3000: ${PORT}`);
+});
+
